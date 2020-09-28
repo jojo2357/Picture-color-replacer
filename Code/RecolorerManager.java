36e013bbb8ad6a786849882e700bfa3f1c710b7e
jojo2357 <66704796+jojo2357@users.noreply.github.com>
2020-09-27 21:28:38 -0700

    Added jpg support

--- Code/RecolorerManager.java
+++ Code/RecolorerManager.java
@@ -10,8 +10,8 @@ class RecolorerManager{
 		//java ColorSeparation InputImage.ext OutputImage ext targetR targetG targetB convertR convertG convertB <optional-not shown>convert Alpha
         	Runtime.getRuntime().exec("java ColorSeparation Bolt.png WhiteLightning png 255 216 0 255 255 255");
         	Runtime.getRuntime().exec("java ColorSeparation Bolt.png WhiteLightning jpg 255 216 0 255 255 255");
-        	Runtime.getRuntime().exec("java ColorSeparation Test.png WhiteLightning png 255 216 0 255 255 255");
-        	Runtime.getRuntime().exec("java ColorSeparation Test.png WhiteLightning jpg 255 216 0 255 255 255");
+        	Runtime.getRuntime().exec("java ColorSeparation Test.png TestLightning png 255 216 0 255 255 255");
+        	Runtime.getRuntime().exec("java ColorSeparation Test.png TestLightning jpg 255 216 0 255 255 255");
 		System.out.print("Finished");
 	}
 }
